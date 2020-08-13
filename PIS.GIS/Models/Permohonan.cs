63803dacb34a6daf6f531a7cbb6a18a38b12985d
Khairil Rizal <manjacat@gmail.com>
2020-08-13 18:13:22 +0800

    Work in Progress
    WIP setakat ini

---
+++ PIS.GIS/Models/Permohonan.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace PIS.GIS.Models
+{
+    public class Permohonan
+    {
+        public int ID { get; set; }
+
+        [Display(Name = "Nama Pemohon")]
+        public string NamaPemohon { get; set; }
+
+        [Display(Name = "Emel")]
+        public string Emel { get; set; }
+
+        [Display(Name = "IPD / Kontinjen")]
+        public string IpdKontinjen { get; set; }
+
+        [Display(Name = "Nama Cadangan")]
+        public string NamaCadangan { get; set; }
+    }
+}
